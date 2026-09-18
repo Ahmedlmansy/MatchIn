1ae1868171c61e97a457ba151462c63a390b3631
Ahmed <ahmedelmansy579@gmail.com>
2026-09-18 16:29:54 +0300

    feat: refactor routing structure by separating auth and dashboard routes into dedicated files

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,13 +1,10 @@
 import DashboardLayout from "@/components/layouts/auth/DashboardLayout/DashboardLayout";
 import MainLayout from "@/components/layouts/auth/MainLayout";
-import LoginPage from "@/features/Auth/LoginPage/LoginPage";
-import RegisterPage from "@/features/Auth/registerPage/RegisterPage";
-import SetNewPassword from "@/features/Auth/setNewPassword/pages/SetNewPassword";
 import HomePage from "@/features/HomePage/HomePage";
 import NotFoundPage from "@/features/NotFoundPage/NotFoundPage";
-import Overview from "@/features/userDashboard/overview/Overview";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import { auth } from "./routes/auth.routes";
+import { dashboard } from "./routes/dashboard.routes";
 
 export const router = createBrowserRouter([
   {
@@ -18,25 +15,15 @@ export const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       {
         path: "/auth",
-        children: [
-          { path: "login", element: <LoginPage /> },
-          { path: "register", element: <RegisterPage /> },
-          { path: "set-new-password", element: <SetNewPassword /> },
-        ],
+        children: [...auth],
       },
     ],
   },
   {
     path: "/dashboard",
     element: <DashboardLayout userName="Alex Mercer" userRole="Senior Dev" />,
-    children: [
-      {
-        index: true,
-        element: <Overview />,
-      },
-    ],
+    children: [...dashboard],
   },
-
   {
     path: "*",
     element: <NotFoundPage />,
---
+++ src/app/routes/auth.routes.js
@@ -0,0 +1,9 @@
+import LoginPage from "@/features/Auth/LoginPage/LoginPage";
+import RegisterPage from "@/features/Auth/registerPage/RegisterPage";
+import SetNewPassword from "@/features/Auth/setNewPassword/pages/SetNewPassword";
+
+export const auth = [
+          { path: "login", element: <LoginPage /> },
+          { path: "register", element: <RegisterPage /> },
+          { path: "set-new-password", element: <SetNewPassword /> },
+        ]
