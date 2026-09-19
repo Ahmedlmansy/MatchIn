f8c412dff7d2a37ef6466efdcf765fdded93a5b8
Ahmed <ahmedelmansy579@gmail.com>
2026-09-19 22:43:15 +0300

    fix : v

---
+++ src/features/roadmap/roadmapDetails/RoadmapDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RoadmapPhaseHeader from './components/RoadmapPhaseHeader'
+
+export default function RoadmapDetails() {
+  return (
+    <div>
+      <RoadmapPhaseHeader/>
+    </div>
+  )
+}
